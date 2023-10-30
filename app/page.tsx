28ab1dc0077cc3f5c3f930648b9499c739de21bc
Robby Ardison <101854553+robbieardison@users.noreply.github.com>
2023-10-31 00:11:10 +0700

    Create CustomButton component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import { Hero } from '@/components'
 import Image from 'next/image'
-import Hero from '@/components/Hero'
+
 
 export default function Home() {
   return (
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,12 @@
-import Image from 'next/image'
+"use client";
 
-import React from 'react'
+import Image from 'next/image'
+import { CustomButton } from '.'
 
 const Hero = () => {
+  const handleScroll = () => {
+
+  }
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
@@ -12,6 +16,11 @@ const Hero = () => {
         <p className='hero__subtitle'>
         Sederhanakan pengalaman sewa mobil Anda dengan proses pemesanan kami yang mudah.
         </p>
+        <CustomButton 
+        title="Explore Cars"
+        containerStyles="bg-primary-blue text-white rounded-full mt-10"
+        handleClick={() => {handleScroll}}
+        />
       </div>
     </div>
   )
